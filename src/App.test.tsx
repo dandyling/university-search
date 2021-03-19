3655055508cd4bf4df59457bb5d6faa91ec84aaf
Dandy Ling <dandyling@gmail.com>
2021-03-20 00:49:20 +0800

    test: :white_check_mark: Add tests
    1. EmptyScreen
    2. ErrorScreen
    3. CenterSpinner

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,24 @@
-import React from "react"
-import { screen } from "@testing-library/react"
-import { render } from "./test-utils"
-import { App } from "./App"
+import { screen } from "@testing-library/react";
+import React from "react";
+import { CenterSpinner } from "./components/CenterSpinner";
+import { EmptyScreen } from "./components/EmptyScreen";
+import { ErrorScreen } from "./components/ErrorScreen";
+import { render } from "./test-utils";
 
-test("renders learn react link", () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn chakra/i)
-  expect(linkElement).toBeInTheDocument()
-})
+test("renders empty screen correctly", () => {
+  render(<EmptyScreen message={"Message"} />);
+  const linkElement = screen.getByText(/Message/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders error screen correctly", () => {
+  render(<ErrorScreen message={"Message"} />);
+  const linkElement = screen.getByText(/Message/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders center spinner", () => {
+  render(<CenterSpinner />);
+  const linkElement = screen.getByText(/Loading.../i);
+  expect(linkElement).toBeInTheDocument();
+});
